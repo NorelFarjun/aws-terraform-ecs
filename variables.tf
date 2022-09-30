b2a066cde84ae55b46398b2951fb9fdcd4e17523
NorelFarjun <46647734+NorelFarjun@users.noreply.github.com>
2022-09-30 21:20:08 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 
 
 variable "region"{} 
-variable "availability _zone_a"{} 
+variable "availability_zone_a"{} 
 variable "availability_zone_b"{} 
 variable "availability_zone_b"{} 
 variable "project_name"{}
